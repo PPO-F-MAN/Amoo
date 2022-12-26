9d946787d967a8ab6b502929e8d60ff8cadc28ce
junghyeonsu <jung660317@naver.com>
2022-12-26 16:48:36 +0900

    chore: eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  extends: ["prettier"],
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "react",
+    "react-hooks",
+    'jsx-a11y',
+    "prettier",
+    "json-format",
+    "simple-import-sort",
+  ],
+  ignorePatterns: [".eslintrc.js", "public", "node_modules", ".cache", ".vscode"],
+  rules: {
+    "no-unused-vars": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+        disallowTypeAnnotations: false,
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        semi: true,
+        tabWidth: 2,
+        useTabs: false,
+        trailingComma: "all",
+        printWidth: 100,
+        arrowParens: "always",
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+};
