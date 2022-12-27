2c17313b320d4a6521e4e4bea76895425d872daf
junghyeonsu <jung660317@naver.com>
2022-12-28 02:34:35 +0900

    chore: breakpoint 설정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,4 +9,12 @@ export const theme = extendTheme({
       'Pretendard, -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
     body: 'Pretendard, -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
   },
+  breakpoints: {
+    // base: ~ 320px,
+    sm: "320px", // 320px ~ 768px
+    md: "768px", // 768px ~ 960px
+    lg: "960px", // 960px ~ 1200px
+    xl: "1200px", // 1200px ~ 1536px
+    "2xl": "1536px", // 1536px ~
+  },
 });
