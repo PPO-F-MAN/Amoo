3d12c53b81f553b294bc1b18911812b44813b522
junghyeonsu <jung660317@naver.com>
2022-12-28 15:50:24 +0900

    feat: 방향키 게임 1차 완성

--- src/pages/game/arrow.tsx
+++ src/pages/game/arrow.tsx
@@ -1,39 +1,79 @@
-import { useEffect, useState } from "react";
+import { Center, Flex, Heading, Text } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 // 요구사항
 // 1. currentArrow는 좌, 우 둘 중 하나로 랜덤으로 나온다.
 // 2. currentArrow가 좌일 때는 좌측 화살표를, 우일 때는 우측 화살표를 누르면 combo가 1씩 증가한다.
 
+interface Item {
+  id: number;
+  direction: "left" | "right";
+}
+
+const LENGTH = 5;
+const add = (id: number): Item => ({ direction: Math.random() > 0.5 ? "left" : "right", id });
+
 const Arrow = () => {
   const [combo, setCombo] = useState<number>(0);
-  const [currentArrow, setCurrentArrow] = useState<"left" | "right">("left");
+  const count = useRef<number>(0);
+  const [currentArrowArray, setCurrentArrowArray] = useState<Item[]>(
+    Array.from({ length: LENGTH }, () => add(count.current++)),
+  );
 
   useEffect(() => {
     const keyDownHandler = (e: KeyboardEvent) => {
-      if (e.key === "ArrowLeft" && currentArrow === "left") {
+      if (e.key === "ArrowLeft" && currentArrowArray[LENGTH - 1].direction === "left") {
         setCombo((prev) => prev + 1);
-        setCurrentArrow(Math.random() > 0.5 ? "left" : "right");
+        currentArrowArray.pop();
+        setCurrentArrowArray([add(count.current++), ...currentArrowArray]);
       }
 
-      if (e.key === "ArrowRight" && currentArrow === "right") {
+      if (e.key === "ArrowRight" && currentArrowArray[LENGTH - 1].direction === "right") {
         setCombo((prev) => prev + 1);
-        setCurrentArrow(Math.random() > 0.5 ? "left" : "right");
+        currentArrowArray.pop();
+        setCurrentArrowArray([add(count.current++), ...currentArrowArray]);
       }
     };
 
     window.addEventListener("keydown", keyDownHandler);
-
-    return () => {
-      window.removeEventListener("keydown", keyDownHandler);
-    };
-  }, [currentArrow]);
+    return () => window.removeEventListener("keydown", keyDownHandler);
+  }, [currentArrowArray]);
 
   return (
-    <div>
-      <h1>Arrow</h1>
-      <h2>Combo: {combo}</h2>
-      <h2>Current Arrow: {currentArrow}</h2>
-    </div>
+    <Center height="100vh" flexDirection="column">
+      <Heading>Arrow Game</Heading>
+
+      <Flex justifyContent="space-evenly" direction="column" height="70%">
+        <AnimatePresence mode="popLayout">
+          {currentArrowArray.map(({ direction, id }) => (
+            <motion.div
+              layout
+              key={id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, translateX: direction === "left" ? -100 : 100 }}
+              transition={{ duration: 0.2, type: "spring" }}
+            >
+              <Flex
+                borderRadius="8px"
+                justifyContent="center"
+                alignItems="center"
+                border="1px solid black"
+                margin="10px"
+                width="50px"
+                height="50px"
+                fontSize="24px"
+              >
+                {direction === "left" ? "←" : "→"}
+              </Flex>
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </Flex>
+
+      <Text>Combo: {combo}</Text>
+    </Center>
   );
 };
 
