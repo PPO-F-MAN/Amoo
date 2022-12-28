7439a8b3a58d129e305ae111a37a05622d12cd92
deli-ght <minjju0660@gmail.com>
2022-12-29 00:47:35 +0900

    feat: proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,14 @@ export default defineConfig({
   plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })],
   server: {
     host: true,
+    proxy: {
+      "/api": {
+        target: "https://random-word-api.herokuapp.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
     watch: {
       usePolling: true,
     },
