612bab939f01dbadb5d98c392ad40eb6db194e6e
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-12-28 17:41:49 +0900

    feat: 숫자 누르기 게임 구현

---
+++ src/components/CountNumbers/utils/index.ts
@@ -0,0 +1,10 @@
+const getShuffledArray = (size: number) => {
+  const shuffledArray = Array.from({ length: size }, (_, i) => i + 1);
+  for (let cur = 0; cur < shuffledArray.length; cur++) {
+    let tmp = Math.floor(Math.random() * (cur + 1));
+    [shuffledArray[cur], shuffledArray[tmp]] = [shuffledArray[tmp], shuffledArray[cur]];
+  }
+  return shuffledArray;
+};
+
+export { getShuffledArray };
--- src/pages/game/game2.tsx
+++ src/pages/game/game2.tsx
@@ -1,7 +1,24 @@
+import { Heading } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+
+import CountNumber from "../../components/CountNumbers";
+
 const Game2 = () => {
+  const [isTimeOver, setIsTimeOver] = useState(false);
+  const counter = useRef(0);
+
+  useEffect(() => {
+    if (isTimeOver) console.timeEnd("game");
+  }, [isTimeOver]);
+
+  const handleTimeOver = (gameData: [number, boolean][]) => {
+    if (gameData.length && !gameData.filter((data) => data[1] !== true).length) setIsTimeOver(true);
+  };
+
   return (
     <div>
-      <h1>Game 2</h1>
+      <Heading textAlign="center">1 to 50</Heading>
+      <CountNumber counter={counter} handleTimeOver={handleTimeOver} />
     </div>
   );
 };
