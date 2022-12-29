2b46e1df7cc9e4dc29da1661ee257bb19bdad197
deli-ght <minjju0660@gmail.com>
2022-12-30 00:55:42 +0900

    style: 하트 색상스타일 적용

--- src/components/Hangman/Heart.tsx
+++ src/components/Hangman/Heart.tsx
@@ -1,30 +1,26 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import { useAtomValue } from "jotai";
 
 import { lifesAtom } from "../../atoms/hangman";
-import { Primary } from "../../constants";
+import { LIFES, Primary } from "../../constants";
+
+export type PrimaryColorType = keyof typeof Primary;
 
 export const Heart = () => {
   const lifes = useAtomValue(lifesAtom);
 
   return (
     <Flex justifyContent="center" alignItems="center">
-      <Contianer>
+      <Box mt="50px" pos="relative" display="inline-block" w="0" h="200px">
         {Array.from(new Array(lifes), () => "").map((key: string, index: number) => {
-          return <Piece border={Primary[300]} key={`${key}-${index}`} />;
+          const bgColor = (Math.trunc((LIFES - lifes) / 2) + 1) * 100;
+          return <Piece bgColor={bgColor as PrimaryColorType} key={`${key}-${index}`} />;
         })}
-      </Contianer>
+      </Box>
     </Flex>
   );
 };
-
-const Contianer = styled.div`
-  position: relative;
-  display: inline-block;
-  width: 0;
-  height: 200px;
-`;
 
 const Piece = styled.span`
   position: absolute;
@@ -33,89 +29,128 @@ const Piece = styled.span`
   width: 0px;
   height: 0px;
   margin-right: 10px;
-  border-top: 60px solid ${({ border }: { border: string }) => border};
   border-left: 35px solid transparent;
   border-right: 35px solid transparent;
 
   &:nth-of-type(14) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 700) as PrimaryColorType]};
     transform: rotate(30deg);
     top: 36px;
     left: -146px;
   }
 
   &:nth-of-type(13) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 600) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 0;
     left: -114px;
   }
 
   &:nth-of-type(12) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 600) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 72px;
     left: -114px;
   }
 
   &:nth-of-type(11) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 500) as PrimaryColorType]};
     transform: rotate(30deg);
     top: 0;
     left: -82px;
   }
 
   &:nth-of-type(10) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 500) as PrimaryColorType]};
     transform: rotate(30deg);
     top: 72px;
     left: -82px;
   }
 
   &:nth-of-type(9) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 400) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 36px;
     left: -50px;
   }
 
   &:nth-of-type(8) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 400) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 0px;
     left: 14px;
   }
 
   &:nth-of-type(7) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 300) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 36px;
     left: 78px;
   }
 
   &:nth-of-type(6) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 200) as PrimaryColorType]};
     transform: rotate(30deg);
     top: 0;
     left: 46px;
   }
 
   &:nth-of-type(5) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 200) as PrimaryColorType]};
     transform: rotate(30deg);
     top: 36px;
     left: -18px;
   }
 
   &:nth-of-type(4) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 200) as PrimaryColorType]};
     transform: rotate(30deg);
     top: 72px;
     left: 46px;
   }
 
   &:nth-of-type(3) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 100) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 72px;
     left: 14px;
   }
 
   &:nth-of-type(2) {
+    border-top: 60px solid
+      ${({ bgColor }: { bgColor: PrimaryColorType }) =>
+        Primary[(bgColor + 100) as PrimaryColorType]};
     transform: rotate(-30deg);
     top: 108px;
     left: -50px;
   }
 
   &:nth-of-type(1) {
+    border-top: 60px solid ${({ bgColor }: { bgColor: PrimaryColorType }) => Primary[bgColor]};
     transform: rotate(30deg);
     top: 108px;
     left: -18px;
