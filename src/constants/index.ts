fe97a07d5cd79e1dfdfc8cd7abbe02d799dfee89
deli-ght <minjju0660@gmail.com>
2022-12-29 22:54:36 +0900

    feat: Primary 컬러 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,3 +7,15 @@ export interface TOAST_OPTIONS {
   duration: number;
   isClosable: boolean;
 }
+
+export const Primary = {
+  100: "#C9F0F8",
+  200: "#ADE8F4",
+  300: "#90E0EF",
+  400: "#48CAE4",
+  500: "#0096C7",
+  600: "#0077B6",
+  700: "#023E8A",
+  800: "#03045E",
+  900: "#050505",
+};
