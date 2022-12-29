66179bda9ed5cb661d608ea9b31fc3506015eb0e
junghyeonsu <jung660317@naver.com>
2022-12-29 22:16:11 +0900

    style: game list page svg 속성 변경

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -37,15 +37,15 @@ const GameListPage = () => {
           top: "-95px",
         }}
         initial={{
-          y: -44,
+          y: -80,
         }}
         animate={{
           y: 0,
         }}
         transition={{
           type: "spring",
-          stiffness: 260,
-          duration: 2,
+          stiffness: 220,
+          duration: 2.5,
         }}
       >
         <svg
@@ -55,11 +55,11 @@ const GameListPage = () => {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <line x1="16" y1="-4.37114e-08" x2="16" y2="129" stroke="white" stroke-width="2" />
+          <line x1="16" y1="-4.37114e-08" x2="16" y2="129" stroke="white" strokeWidth="2" />
           <path
             d="M14.91 129.877L16 130.585L17.09 129.877L24 125.385L30 129.285V137.515L22.91 142.123L16 146.615L9.08998 142.123L2 137.515V129.285L8 125.385L14.91 129.877Z"
             stroke="white"
-            stroke-width="4"
+            strokeWidth="4"
           />
         </svg>
       </motion.div>
