4c76bb632fb721c6c24d2b26ddf37212b464d8a3
deli-ght <minjju0660@gmail.com>
2022-12-29 16:02:23 +0900

    refactor: toast 문구 정리

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,9 @@
 export * from "./hangman";
+
+type TOAST_MESSAGES = "이미 입력된 답입니다!" | "오답입니다!";
+export interface TOAST_OPTIONS {
+  title: TOAST_MESSAGES;
+  status: "info" | "warning" | "success" | "error" | "loading";
+  duration: number;
+  isClosable: boolean;
+}
