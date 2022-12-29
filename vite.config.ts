ebb6890f2dc2f8a0022701b52b885469bc8894da
deli-ght <minjju0660@gmail.com>
2022-12-30 00:20:04 +0900

    feat: svgr 플러그인 설치

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import react from "@vitejs/plugin-react";
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })],
+  plugins: [
+    svgr({
+      exportAsDefault: true,
+    }),
+    react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
+  ],
   server: {
     host: true,
     proxy: {
