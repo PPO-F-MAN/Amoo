9804f09a1bd06401386c982dd81af77237f1fb77
junghyeonsu <jung660317@naver.com>
2022-12-30 09:37:40 +0900

    refactor: HangHeart 생성

---
+++ src/components/common/HangHeart.tsx
@@ -0,0 +1,64 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const HangHeart = () => {
+  const navigate = useNavigate();
+  const [isMounted, setIsMounted] = useState<boolean>(true);
+
+  const moveToHomeAfterAnimate = () => {
+    setIsMounted(false);
+    setTimeout(() => {
+      navigate("/");
+    }, 400);
+  };
+
+  return (
+    <AnimatePresence>
+      {isMounted && (
+        <motion.div
+          style={{
+            position: "absolute",
+            left: "20px",
+            top: "-95px",
+          }}
+          whileHover={{
+            cursor: "pointer",
+          }}
+          initial={{
+            y: -80,
+          }}
+          animate={{
+            y: 0,
+          }}
+          transition={{
+            type: "spring",
+            stiffness: 220,
+            duration: 2.5,
+          }}
+          exit={{
+            y: -100,
+          }}
+          onClick={moveToHomeAfterAnimate}
+        >
+          <svg
+            width="32"
+            height="149"
+            viewBox="0 0 32 149"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line x1="16" y1="-4.37114e-08" x2="16" y2="129" stroke="white" strokeWidth="2" />
+            <path
+              d="M14.91 129.877L16 130.585L17.09 129.877L24 125.385L30 129.285V137.515L22.91 142.123L16 146.615L9.08998 142.123L2 137.515V129.285L8 125.385L14.91 129.877Z"
+              stroke="white"
+              strokeWidth="4"
+            />
+          </svg>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default HangHeart;
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,8 +1,8 @@
 import { Flex, Heading, Img } from "@chakra-ui/react";
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.png";
+import HangHeart from "../../components/common/HangHeart";
 
 const GameListPage = () => {
   return (
@@ -30,40 +30,7 @@ const GameListPage = () => {
           cursor: "pointer",
         }}
       />
-      <motion.div
-        style={{
-          position: "absolute",
-          left: "20px",
-          top: "-95px",
-        }}
-        initial={{
-          y: -80,
-        }}
-        animate={{
-          y: 0,
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 220,
-          duration: 2.5,
-        }}
-      >
-        <svg
-          width="32"
-          height="149"
-          viewBox="0 0 32 149"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <line x1="16" y1="-4.37114e-08" x2="16" y2="129" stroke="white" strokeWidth="2" />
-          <path
-            d="M14.91 129.877L16 130.585L17.09 129.877L24 125.385L30 129.285V137.515L22.91 142.123L16 146.615L9.08998 142.123L2 137.515V129.285L8 125.385L14.91 129.877Z"
-            stroke="white"
-            strokeWidth="4"
-          />
-        </svg>
-      </motion.div>
-
+      <HangHeart />
       <Link style={{ zIndex: 1 }} to="/game/left-right">
         <Heading size={{ base: "2xl", lg: "3xl" }} color="white">
           left & right
