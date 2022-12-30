362af4191c84eb69f737384e44fe0604e8215eeb
junghyeonsu <jung660317@naver.com>
2022-12-30 16:34:32 +0900

    feat: max combo 구현

--- src/atoms/left-right.ts
+++ src/atoms/left-right.ts
@@ -2,6 +2,7 @@ import { atom } from "jotai";
 import { nanoid } from "nanoid";
 
 import { ARROW_LENGTH, DELAY } from "../constants/left-right";
+import { safeLocalStorage } from "../utils/storage";
 
 interface Arrow {
   id: string;
@@ -33,6 +34,7 @@ export const shakeAtom = atom<boolean>(false);
 export const scoreAtom = atom<number>(0);
 
 export const comboAtom = atom<number>(0);
+export const maxComboAtom = atom<number>(Number(safeLocalStorage.get("maxCombo")) || 0);
 
 export const arrowsAtom = atom<Arrow[]>([...Array(ARROW_LENGTH)].map(() => addArrow()));
 
@@ -41,8 +43,17 @@ export const lastArrowAtom = atom<Arrow>((get) => get(arrowsAtom)[ARROW_LENGTH -
 export const correctAtom = atom(null, (get, set) => {
   if (get(delayAtom)) return;
 
-  set(scoreAtom, get(scoreAtom) + 1);
-  set(comboAtom, get(comboAtom) + 1);
+  const maxCombo = get(maxComboAtom);
+  const combo = get(comboAtom);
+  const score = get(scoreAtom);
+
+  if (combo + 1 > maxCombo) {
+    set(maxComboAtom, combo + 1);
+    safeLocalStorage.set("maxCombo", (combo + 1).toString());
+  }
+
+  set(scoreAtom, score + 1);
+  set(comboAtom, combo + 1);
   set(timeAtom, (prevTime) => prevTime + 10);
   set(arrowsAtom, [addArrow(), ...get(arrowsAtom).slice(0, ARROW_LENGTH - 1)]);
   set(delayAtom, true);
--- src/components/LeftRight/index.ts
+++ src/components/LeftRight/index.ts
@@ -2,6 +2,7 @@ export { default as Arrow } from "./Arrow";
 export { default as Combo } from "./Combo";
 export { default as HorizontalLine } from "./HorizontalLine";
 export { default as LastArrowHighlighter } from "./LastArrowHighlighter";
+export { default as MaxCombo } from "./MaxCombo";
 export { default as MobilePad } from "./MobilePad";
 export { default as Score } from "./Score";
 export { default as Timer } from "./Timer";
