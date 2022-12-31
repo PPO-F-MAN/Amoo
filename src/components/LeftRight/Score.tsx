267365c638e86eb64767f199f1e811d73411c4e4
junghyeonsu <jung660317@naver.com>
2022-12-31 13:02:42 +0900

    chore: animiation 임시제거

--- src/components/LeftRight/Score.tsx
+++ src/components/LeftRight/Score.tsx
@@ -1,8 +1,8 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { useAtomValue } from "jotai";
 import { memo } from "react";
-import AnimatedNumbers from "react-animated-numbers";
 
+// import AnimatedNumbers from "react-animated-numbers";
 import { scoreAtom } from "../../atoms/left-right";
 
 const Score = () => {
@@ -19,8 +19,11 @@ const Score = () => {
       <Text color="white" lineHeight="0.9" fontSize="20px" fontWeight="bold">
         SCORE
       </Text>
+      <Text color="white" lineHeight="0.9" fontSize="20px" fontWeight="bold">
+        {score}
+      </Text>
 
-      <AnimatedNumbers
+      {/* <AnimatedNumbers
         fontStyle={{
           color: "white",
           lineHeight: "0.9",
@@ -28,7 +31,7 @@ const Score = () => {
           fontWeight: "bold",
         }}
         animateToNumber={score}
-      />
+      /> */}
     </Flex>
   );
 };
