2439c5408ab250a2443e7605fdf4d4dd1c9c0af5
deli-ght <minjju0660@gmail.com>
2023-01-06 18:26:25 +0900

    feat: 404 페이지 UI 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,27 @@
+import { Button, Container, Heading } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const navigateMain = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>404</h1>
-    </div>
+    <Container
+      color={"primary.100"}
+      display="flex"
+      flexDirection="column"
+      h="100vh"
+      justifyContent={"center"}
+      centerContent
+    >
+      <Heading>잘못된 페이지 입니다.</Heading>
+      <Button mt="30" colorScheme={"primary"} onClick={navigateMain}>
+        메인 화면으로 이동
+      </Button>
+    </Container>
   );
 };
 
