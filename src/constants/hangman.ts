6221b37e43368805ba88e22676c6bbd4c266b25a
deli-ght <minjju0660@gmail.com>
2023-01-06 17:16:03 +0900

    refactor : toast 삭제

--- src/constants/hangman.ts
+++ src/constants/hangman.ts
@@ -1,17 +1,2 @@
-import type { TOAST_OPTIONS } from ".";
-
 export const LIFES: number = 14;
 export const LENGTH_OF_WORD: number = 7;
-
-export const TOAST_SUBMITTED: Partial<TOAST_OPTIONS> = {
-  title: "이미 입력된 답입니다!",
-  duration: 3000,
-  isClosable: true,
-};
-
-export const TOAST_WRONG: Partial<TOAST_OPTIONS> = {
-  title: "오답입니다!",
-  status: "warning",
-  duration: 3000,
-  isClosable: true,
-};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,5 @@
 export * from "./hangman";
 
-type TOAST_MESSAGES = "이미 입력된 답입니다!" | "오답입니다!";
-export interface TOAST_OPTIONS {
-  title: TOAST_MESSAGES;
-  status: "info" | "warning" | "success" | "error" | "loading";
-  duration: number;
-  isClosable: boolean;
-}
-
 export const Primary = {
   100: "#C9F0F8",
   200: "#ADE8F4",
